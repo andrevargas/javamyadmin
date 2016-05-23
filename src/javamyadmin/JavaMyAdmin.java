a179d3257e991ef958b0786c259f41aa56703439
André Vargas <vargas@geosapiens.com.br>
2016-05-23 16:34:57 -0300

    Removidos comentários desnecessários no main.

--- src/javamyadmin/JavaMyAdmin.java
+++ src/javamyadmin/JavaMyAdmin.java
@@ -5,8 +5,6 @@
  */
 package javamyadmin;
 
-import com.bethecoder.ascii_table.ASCIITable;
-
 /**
  *
  * @author 5108055
@@ -20,28 +18,6 @@ public class JavaMyAdmin {
         
         AppControler app = new AppControler();
         app.start();
-
-//        String [] header = { "User Name", "Salary", "Designation", "Address", "Lucky#" };
-//        
-//        String[][] data = {
-//            { "Ram", "2000", "Manager", "#99, Silk board", "1111"  },
-//            { "Sri", "12000", "Developer", "BTM Layout", "22222" },
-//            { "Prasad", "42000", "Lead", "#66, Viaya Bank Layout", "333333" },
-//            { "Anu", "132000", "QA", "#22, Vizag", "4444444" },
-//            { "Sai", "62000", "Developer", "#3-3, Kakinada"  },
-//            { "Venkat", "2000", "Manager"   },
-//            { "Raj", "62000"},
-//            { "BTC"},
-//        };
-//        
-//        ASCIITable.getInstance().printTable(header, data);
-
-//        String[] header = sgbd.getDatabases().get(0).getTables().get(2).getHeaderAsArray();
-//        String[][] data = sgbd.getDatabases().get(0).getTables().get(2).getRowsAsArray();
-//        
-//        System.out.println("Connected on database " + sgbd.getDatabases().get(0).getName());
-//        System.out.println("Table: " + sgbd.getDatabases().get(0).getTables().get(2).getName());
-        
         
     }
     
